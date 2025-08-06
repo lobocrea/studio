22cc9823bd51040646b77893a13217b10ce08ab8
Luis Guerra .•. <lobocreaweb@gmail.com>
2025-08-06 03:28:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Moon, Sun } from 'lucide-react';
-import { useTheme } from './theme-provider';
+import { useTheme } from 'next-themes';
 import { Button } from './ui/button';
 
 export function ThemeSwitcher() {
